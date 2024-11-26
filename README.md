ce0fb13cc3b5bbf0627aff159c1cd78fb53bc18c
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2024-11-26 17:38:50 +0800

    Added icons

---
+++ README.md
@@ -0,0 +1,3 @@
+## Acknowledgements
+- <a href="https://www.flaticon.com/free-icons/mosque" title="mosque icons">Mosque icons created by Icon home - Flaticon</a>
+- <a href="https://www.flaticon.com/free-icons/pray" title="pray icons">Pray icons created by Freepik - Flaticon</a>
--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 let map;
-
+let layerControl
 document.addEventListener("DOMContentLoaded", (event) => {
     map = L.map('map').setView([1.3521, 103.8198], 12);
 
@@ -7,23 +7,47 @@ document.addEventListener("DOMContentLoaded", (event) => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    
+    layerControl = L.control.layers().addTo(map);
     loadMosques();
+    loadMusollah();
 });
 
-function loadMosques(){
-    var layerControl = L.control.layers().addTo(map);
+function loadMosques() {
+    var moqueIcon = L.icon({
+        iconUrl: 'icons/mosque.png',
+
+        iconSize: [36, 36], // size of the icon
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
     var mosqueMarkers = new L.MarkerClusterGroup();
-    // const axios = require('axios');
     axios.get("/data/mosque.json")
-    .then(function(response){
-        console.log(response.data);
-        let data = response.data;
-        for(const p in data){
-            mosqueMarkers.addLayer(L.marker([data[p]["coordinates"][0], data[p]["coordinates"][1]]));
-        }
-        map.addLayer(mosqueMarkers);
-        // let mosques = L.layerGroup(mosqueMarkers);
-        layerControl.addOverlay(mosqueMarkers, "Mosques");
-    })
+        .then(function (response) {
+            console.log(response.data);
+            let data = response.data;
+            for (const p in data) {
+                mosqueMarkers.addLayer(L.marker([data[p]["coordinates"][0], data[p]["coordinates"][1]], { icon: moqueIcon }));
+            }
+            map.addLayer(mosqueMarkers);
+            layerControl.addOverlay(mosqueMarkers, "Mosques");
+        })
+}
+
+function loadMusollah() {
+    var musollahIcon = L.icon({
+        iconUrl: 'icons/praying.png',
+
+        iconSize: [36, 36], // size of the icon
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+    var musollahMarkers = new L.MarkerClusterGroup();
+    axios.get("/data/musollah.json")
+        .then(function (response) {
+            console.log(response.data);
+            let data = response.data;
+            for (const p in data) {
+                musollahMarkers.addLayer(L.marker([data[p]["coordinates"][0], data[p]["coordinates"][1]],{icon:musollahIcon}));
+            }
+            map.addLayer(musollahMarkers);
+            layerControl.addOverlay(musollahMarkers, "Musollah");
+        })
 }
