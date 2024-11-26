56f07f7afe7b55e7bab2f84415d7c331e96da6a1
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2024-11-26 14:46:06 +0800

    first commit

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([1.3521, 103.8198], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
